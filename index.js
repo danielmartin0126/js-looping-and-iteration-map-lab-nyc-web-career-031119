1453b5758eba4eeced9932435c21737be59a29ce
Danny Martin <danielmartin0126@gmail.com>
2019-04-23 18:24:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(arr) {
   const newArr = arr.map(function(eng) {
-    return Object.assign({}, eng, { equipment: 'Laptop' });
+    return Object.assign({}, driver, { name: driver["name"].toLowerCase() });
 });
 }
