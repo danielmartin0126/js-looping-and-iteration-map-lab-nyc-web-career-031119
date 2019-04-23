8efb4e9d7874adf3c95c48bd09df5eedf17e3300
Danny Martin <danielmartin0126@gmail.com>
2019-04-23 18:23:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(arr) {
-  const equippedEngineers = newEngineers.map(function(eng) {
+  const new = arr.map(function(eng) {
     return Object.assign({}, eng, { equipment: 'Laptop' });
 });
 }
